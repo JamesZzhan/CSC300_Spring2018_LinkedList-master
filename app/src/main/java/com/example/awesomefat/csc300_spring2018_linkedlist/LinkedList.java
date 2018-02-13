15b5957a8c9c673a0b82d2fd1fb4bd1537ce6c25
JamesZzhan <33325455+JamesZzhan@users.noreply.github.com>
2018-02-12 22:35:02 -0600

    Initial commit

---
+++ app/src/main/java/com/example/awesomefat/csc300_spring2018_linkedlist/LinkedList.java
@@ -0,0 +1,162 @@
+package com.example.awesomefat.csc300_spring2018_linkedlist;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by awesomefat on 1/30/18.
+ */
+
+public class LinkedList
+{
+    private Node head;
+    private ViewGroup linkedListContainer;
+    private Context theContext;
+
+    public LinkedList(ViewGroup linkedListContainer, Context theContext)
+    {
+        this.head = null;
+        this.linkedListContainer = linkedListContainer;
+        this.theContext = theContext;
+    }
+
+    public int removeFront() throws Exception
+    {
+        //removes the front of the linked list, and updates the interface
+        //and ultimately returns the int payload of the node that was removed.
+        if(this.head != null)
+        {
+            Node node2Remove = this.head;
+            this.head = this.head.getNextNode();
+            node2Remove.setNextNode(null);
+            this.linkedListContainer.removeViewAt(0); // removes the view at 0
+            return node2Remove.getPayload();
+        }
+        else
+        {
+            Toast.makeText(this.theContext, "List is Empty", Toast.LENGTH_SHORT).show();
+            throw new Exception("Empty List");
+        }
+    }
+
+    public int removeEnd() throws Exception
+    {
+        //removes the end of the linked list, and updates the interface
+        //and ultimately returns the int payload of the node that was removed.
+        if(this.head != null)
+        {
+            //traverse the list to find the node right before the last node
+            Node node2Remove = this.head;
+            while(node2Remove.getNextNode() != null)
+            {
+                node2Remove = node2Remove.getNextNode();
+            }
+            //we know that node2Remove points to the very last node
+            //find the node that points to node2Remove
+            if(node2Remove == this.head)
+            {
+                //we have a one list
+                this.head = null;
+            }
+            else
+            {
+                Node prevNode = this.head;
+                while(prevNode.getNextNode() != node2Remove)
+                {
+                    prevNode = prevNode.getNextNode();
+                }
+                //prevNode points to the node right before node2Remove
+                prevNode.setNextNode(null);
+
+            }
+            this.linkedListContainer.removeViewAt(this.linkedListContainer.getChildCount()-1);
+            return node2Remove.getPayload();
+        }
+        else
+        {
+            //we have an empty list
+            Toast.makeText(this.theContext,"Empty List", Toast.LENGTH_SHORT).show();
+            throw new Exception("Empty List");
+        }
+    }
+
+    public void addFront(int payload)
+    {
+        Node n = new Node(payload);
+
+        //add to front of list
+        if(this.head == null)
+        {
+            this.head = n;
+        }
+        else
+        {
+            //not dealing with the empty list
+            n.setNextNode(this.head);
+            this.head = n;
+        }
+
+        //update the interface
+        TextView tv = new TextView(this.theContext);
+        tv.setText("" + payload);
+        tv.setGravity(Gravity.CENTER);
+        this.linkedListContainer.addView(tv,0);
+    }
+
+    public void addEnd(int payload)
+    {
+        if(this.head == null)
+        {
+            this.addFront(payload);
+        }
+        else
+        {
+            this.head.addEnd(payload);
+            /*
+            Node n = new Node(payload);
+            Node currNode = this.head;
+
+            while(currNode.getNextNode() != null)
+            {
+                currNode = currNode.getNextNode();
+            }
+            //currnode is sitting at the last node
+            currNode.setNextNode(n);
+            */
+
+            //update the interface
+            TextView tv = new TextView(this.theContext);
+            tv.setText("" + payload);
+            tv.setGravity(Gravity.CENTER);
+            this.linkedListContainer.addView(tv);
+        }
+    }
+
+    public void display()
+    {
+        if(this.head == null)
+        {
+            System.out.println("Empty List");
+        }
+        else
+        {
+            //do stuff here
+            this.head.display();
+            System.out.println("");
+            /*
+            String answer = "";
+            Node currNode = this.head;
+            while(currNode != null)
+            {
+                answer = answer + currNode.getPayload() + " -> ";
+                currNode = currNode.getNextNode();
+            }
+            System.out.println(answer);
+            */
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/awesomefat/csc300_spring2018_linkedlist/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.awesomefat.csc300_spring2018_linkedlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+    private EditText newNumberET;
+    //private Button addFrontButton;
+    //private Button addEndButton;
+    private ViewGroup linkedListContainer;
+    private LinkedList ll;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.newNumberET = (EditText)this.findViewById(R.id.newNumberET);
+        //this.addFrontButton = (Button)this.findViewById(R.id.addFrontButton);
+        //this.addEndButton = (Button)this.findViewById(R.id.addEndButton);
+        this.linkedListContainer = (ViewGroup)this.findViewById(R.id.linkedListContainer);
+
+        this.ll = new LinkedList(this.linkedListContainer, this);
+        this.ll.display();
+    }
+
+    /*
+    public void genericButtonOnClick(View v)
+    {
+        if(v == this.addFrontButton)
+        {
+            System.out.println("Add Front Button Pressed");
+        }
+        else
+        {
+            System.out.println("Add End Button Pressed");
+        }
+    }
+    */
+
+    public void addFrontButtonClicked(View v)
+    {
+        String temp = this.newNumberET.getText().toString();
+        this.newNumberET.setText("");
+        this.ll.addFront(Integer.parseInt(temp));
+        //this.ll.display();
+    }
+
+    public void addEndButtonClicked(View v)
+    {
+        String temp = this.newNumberET.getText().toString();
+        this.newNumberET.setText("");
+        this.ll.addEnd(Integer.parseInt(temp));
+        //this.ll.display();
+    }
+
+    public void removeFrontButtonClicked(View v)
+    {
+        try
+        {
+            int value = this.ll.removeFront();
+            //do stuff with that element
+            this.ll.display();
+        }
+        catch(Exception e)
+        {
+            System.err.println("Empty List");
+        }
+    }
+
+    public void removeEndButtonClicked(View v)
+    {
+        try
+        {
+            int value = this.ll.removeEnd();
+            this.ll.display();
+        }
+        catch(Exception e){}
+
+    }
+}
